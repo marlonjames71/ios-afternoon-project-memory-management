02e62a509c8eb099be94178a3b022b73e7b3dd6a
Marlon <marmoneymar@icloud.com>
2019-10-16 18:27:53 -0400

    Storyboard setup

--- Memory-Management/Memory-Management/Models/MJRContact.h
+++ Memory-Management/Memory-Management/Models/MJRContact.h
@@ -1,15 +1,19 @@
 //
-//  ViewController.h
+//  MJRContact.h
 //  Memory-Management
 //
 //  Created by Marlon Raskin on 10/16/19.
 //  Copyright © 2019 Marlon Raskin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJRContact : NSObject
+
 
 
 @end
 
+NS_ASSUME_NONNULL_END
---
+++ Memory-Management/Memory-Management/Models/MJRContact.m
@@ -0,0 +1,13 @@
+//
+//  MJRContact.m
+//  Memory-Management
+//
+//  Created by Marlon Raskin on 10/16/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+#import "MJRContact.h"
+
+@implementation MJRContact
+
+@end
